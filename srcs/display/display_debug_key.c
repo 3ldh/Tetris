2405ab9825c0485dd8f024e10dc4d40bd7cca265
maud.marel@epitech.eu <maud.marel@epitech.eu>
2016-03-07 17:40:47 +0100

    recup direction term

--- srcs/display/display_debug_key.c
+++ srcs/display/display_debug_key.c
@@ -5,32 +5,29 @@
 ** Login   <marel_m@epitech.net>
 ** 
 ** Started on  Mon Mar  7 13:26:56 2016 maud marel
-** Last update Mon Mar  7 13:27:18 2016 maud marel
+** Last update Mon Mar  7 17:32:49 2016 maud marel
 */
 
 #include "tetris.h"
 
-void	check_if_space(char *str)
-{
-  if (my_strcmp(str, " ") == 0)
-    my_putstr("(space)");
-  else
-    my_putstr(str);
-  my_putchar('\n');
-}
-
 void	display_key(t_tetris *tetris)
 {
   my_putstr("Key Left : ");
-  check_if_space(tetris->options->left);
+  my_putstr(tetris->options->left);
+  my_putchar('\n');
   my_putstr("Key Right : ");
-  check_if_space(tetris->options->right);
+  my_putstr(tetris->options->right);
+  my_putchar('\n');
   my_putstr("Key Turn : ");
-  check_if_space(tetris->options->turn);
+  my_putstr(tetris->options->turn);
+  my_putchar('\n');
   my_putstr("Key Drop : ");
-  check_if_space(tetris->options->drop);
+  my_putstr(tetris->options->drop);
+  my_putchar('\n');
   my_putstr("Key Quit : ");
-  check_if_space(tetris->options->quit);
+  my_putstr(tetris->options->quit);
+  my_putchar('\n');
   my_putstr("Key Pause : ");
-  check_if_space(tetris->options->pause);
+  my_putstr(tetris->options->pause);
+  my_putchar('\n');
 }
